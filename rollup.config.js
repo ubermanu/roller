d0d653720c231b692252b774204e8354ed5982de
ubermanu <e.vodor@gmail.com>
2020-12-16 08:50:34 +0000

    Add config for the options page and fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,31 @@
 import copy from 'rollup-plugin-copy'
 import { uglify } from 'rollup-plugin-uglify'
 
-export default {
-    input: 'src/main.js',
-    output: {
-        file: 'dist/bundle.js',
-        format: 'iife',
+export default [
+    {
+        input: 'src/main.js',
+        output: {
+            file: 'dist/bundle.js',
+            format: 'iife',
+        },
+        plugins: [
+            uglify(),
+            copy({
+                targets: [{ src: 'public/*', dest: 'dist' }],
+            }),
+        ],
     },
-    plugins: [
-        uglify(),
-        copy({
-            targets: [{ src: 'public/*', dest: 'dist' }],
-        }),
-    ],
-}
+    {
+        input: 'src/pages/options.js',
+        output: {
+            file: 'dist/pages/options.js',
+            format: 'iife',
+        },
+        plugins: [
+            uglify(),
+            copy({
+                targets: [{ src: 'src/pages/options.html', dest: 'dist/pages' }],
+            }),
+        ],
+    }
+]
--- src/pages/lib/util/object.js
+++ src/pages/lib/util/object.js
@@ -119,5 +119,5 @@ export default Object.freeze({
     deepMerge,
     clone,
     deepClone,
-    set,
+    set: set,
 })
