f830478b76bc6c32ef77591df30c3ba6d72b1a93
ubermanu <e.vodor@gmail.com>
2020-12-16 08:44:44 +0000

    Remove requirejs

--- src/pages/lib/util/name.js
+++ src/pages/lib/util/name.js
@@ -1,38 +1,36 @@
 // TODO: Not quite correct, but it's the best I can do
-define(function () {
-    'use strict'
 
-    var id = 0,
-        sId = '__private__0'
+var id = 0,
+    sId = '__private__0'
 
-    // TODO: s is currently ignored
-    function Name(s) {
-        if (!(this instanceof Name)) {
-            return new Name(s)
-        }
-        s = this[sId] = '__private__' + ++id
-        // TODO maybe remove this
-        this.public = Object.freeze({
-            toString: function () {
-                return s
-            },
-        })
-        Object.freeze(this)
+// TODO: s is currently ignored
+function Name(s) {
+    if (!(this instanceof Name)) {
+        return new Name(s)
     }
-
-    Name.prototype.toString = function () {
-        return this[sId]
-    }
-    // TODO should these be frozen?
-    Object.freeze(Name)
-    Object.freeze(Name.prototype)
-
-    function isName(x) {
-        return x instanceof Name
-    }
-
-    return Object.freeze({
-        Name: Name,
-        isName: isName,
+    s = this[sId] = '__private__' + ++id
+    // TODO maybe remove this
+    this.public = Object.freeze({
+        toString: function () {
+            return s
+        },
     })
+    Object.freeze(this)
+}
+
+Name.prototype.toString = function () {
+    return this[sId]
+}
+
+// TODO should these be frozen?
+Object.freeze(Name)
+Object.freeze(Name.prototype)
+
+function isName(x) {
+    return x instanceof Name
+}
+
+export default Object.freeze({
+    Name,
+    isName,
 })
