2605b37fc8dbc7e27ac1136ad8163a74e6ab89fb
ubermanu <manu@paca.happy-dev.fr>
2021-05-08 11:08:03 +0200

    Format code

--- public/background.js
+++ public/background.js
@@ -1,12 +1,12 @@
 'use strict'
 
 if ('Options.config.user' in localStorage) {
-    var o = JSON.parse(localStorage['Options.config.user'])
-    chrome.storage.local.set(o, function () {
-        delete localStorage['Options.config.user']
-    })
+  const o = JSON.parse(localStorage['Options.config.user'])
+  chrome.storage.local.set(o, function () {
+    delete localStorage['Options.config.user']
+  })
 }
 
 if ('Options.config.base' in localStorage) {
-    delete localStorage['Options.config.base']
+  delete localStorage['Options.config.base']
 }
