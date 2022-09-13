0e0e7cff3a79b7a91aa3bea5892cc3735b15644c
ubermanu <manu@paca.happy-dev.fr>
2022-09-13 19:47:18 +0200

    format file

--- src/OptionsPage.svelte
+++ src/OptionsPage.svelte
@@ -45,42 +45,31 @@
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    <input type="checkbox"
-                           bind:checked={$options.stickyScroll}
-                    />{' '}
+                    <input type="checkbox" bind:checked={$options.stickyScroll}/>{' '}
                     {$_('Scroll without holding down the mouse button')}
                 </label>
             </div>
             <div class="control">
                 {$_('...if moving less than')}{' '}
-                <input
-                        type="number"
-                        class="input is-small"
-                        bind:value={$options.moveThreshold}
-                        aria-label={$_('...if moving less than X pixels')}
-                />{' '}
+                <input type="number" class="input"
+                       bind:value={$options.moveThreshold}
+                       aria-label={$_('...if moving less than X pixels')}/>{' '}
                 {$_('pixels')}
             </div>
         </div>
         <div class="field">
             <div class="control">
                 {$_('Scroll if moving more than')}{' '}
-                <input
-                        type="number"
-                        class="input is-small"
-                        bind:value={$options.dragThreshold}
-                        aria-label={$_('Drag threshold in pixels')}
-                />{' '}
+                <input type="number" class="input"
+                       bind:value={$options.dragThreshold}
+                       aria-label={$_('Drag threshold in pixels')}/>{' '}
                 {$_('pixels')}
             </div>
         </div>
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    <input
-                            type="checkbox"
-                            bind:checked={$options.middleClick}
-                    />{' '}
+                    <input type="checkbox" bind:checked={$options.middleClick}/>{' '}
                     {$_('Scroll by using (Middle Click)')}
                 </label>
             </div>
@@ -88,10 +77,7 @@
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    <input
-                            type="checkbox"
-                            bind:checked={$options.ctrlClick}
-                    />{' '}
+                    <input type="checkbox" bind:checked={$options.ctrlClick}/>{' '}
                     {$_('Scroll by using (Ctrl/⌘ + Left Click)')}
                 </label>
             </div>
@@ -102,20 +88,16 @@
         <div class="field">
             <div class="control">
                 {$_('Move speed:')}{' '}
-                <input type="number"
-                       class="input is-small"
+                <input type="number" class="input"
                        bind:value={$options.moveSpeed}
-                       aria-label={$_('Move speed in pixels')}
-                />{' '}
+                       aria-label={$_('Move speed in pixels')}/>{' '}
                 {$_('(lower is faster)')}
             </div>
         </div>
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    <input type="checkbox"
-                           bind:checked={$options.sameSpeed}
-                    />{' '}
+                    <input type="checkbox" bind:checked={$options.sameSpeed}/>{' '}
                     {$_('Scroll at the same speed (ignore mouse movement)')}
                 </label>
             </div>
@@ -123,15 +105,11 @@
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    <input type="checkbox"
-                           bind:checked={$options.shouldCap}
-                    />{' '}
+                    <input type="checkbox" bind:checked={$options.shouldCap}/>{' '}
                     {$_('Don\'t scroll faster than')}{' '}
-                    <input type="number"
-                           class="input is-small"
+                    <input type="number" class="input"
                            bind:value={$options.capSpeed}
-                           aria-label={$_('Speed cap in pixels')}
-                    />{' '}
+                           aria-label={$_('Speed cap in pixels')}/>{' '}
                     {$_('pixels')}
                 </label>
             </div>
@@ -142,10 +120,7 @@
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    <input type="checkbox"
-                           name="innerScroll"
-                           bind:checked={$options.innerScroll}
-                    />{' '}
+                    <input type="checkbox" bind:checked={$options.innerScroll}/>{' '}
                     {$_('Scroll on inner elements')}
                 </label>
             </div>
@@ -153,10 +128,7 @@
         <div class="field">
             <div class="control">
                 <label class="checkbox">
-                    <input type="checkbox"
-                           name="scrollOnLinks"
-                           bind:checked={$options.scrollOnLinks}
-                    />{' '}
+                    <input type="checkbox" bind:checked={$options.scrollOnLinks}/>{' '}
                     {$_('Scroll when clicking on a link or textarea')}
                 </label>
             </div>
