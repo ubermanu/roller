31a4391cd8c058b10a75bee3efeb45a1cee7c1dc
ubermanu <manu@paca.happy-dev.fr>
2023-02-23 14:47:35 +0100

    minor change

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,7 +2,7 @@ import defaults from './defaultOptions'
 import AutoScroll from './components/AutoScroll.svelte'
 
 const htmlNode = document.documentElement
-let autoScroll = new AutoScroll()
+const autoScroll = new AutoScroll()
 
 // TODO: Restore listener on option changes
 chrome?.storage?.local.get(defaults, (options) => {
