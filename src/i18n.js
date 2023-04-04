b743614851e09e9ab0e0182351c749864025515e
ubermanu <manu@paca.happy-dev.fr>
2023-04-04 11:44:22 +0200

    add translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,29 @@
 import { getBrowserLocale, locale, translations } from 'svelte-intl'
-import fr from './i18n/fr.json'
+import de from './i18n/de_DE.json'
+import es from './i18n/es_ES.json'
+import fr from './i18n/fr_FR.json'
+import it from './i18n/it_IT.json'
+import ja from './i18n/ja_JP.json'
+import ko from './i18n/ko_KR.json'
+import nl from './i18n/nl_NL.json'
+import pt from './i18n/pt_PT.json'
+import ru from './i18n/ru_RU.json'
+import zh from './i18n/zh_Hans_CN.json'
 
 // If you want to split your bundle, you can call this multiple times,
 // the dictionaries will be merged.
 translations.update({
   en: {},
+  de,
+  es,
   fr,
+  it,
+  ja,
+  ko,
+  nl,
+  pt,
+  ru,
+  zh,
 })
 
 // try to use window.navigator.language
