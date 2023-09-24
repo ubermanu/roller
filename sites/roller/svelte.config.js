757f31bd09cf93486d5f368c062ac49cf08b56ae
ubermanu <manu@paca.happy-dev.fr>
2023-09-24 15:34:47 +0200

    feat: deploy to pages on tag

--- sites/roller/svelte.config.js
+++ sites/roller/svelte.config.js
@@ -9,8 +9,10 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
-
   preprocess: vitePreprocess(),
+  paths: {
+    base: process.env.SVELTEKIT_BASE_URL || '',
+  },
 }
 
 export default config
