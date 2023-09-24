02de897c91a28e9042383f44054b28bfaac5d869
ubermanu <manu@paca.happy-dev.fr>
2023-09-24 14:51:44 +0200

    add single page

--- sites/roller/src/routes/+page.svelte
+++ sites/roller/src/routes/+page.svelte
@@ -1,4 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+  import chromeIcon from '$lib/images/chrome.svg'
+  import firefoxIcon from '$lib/images/firefox.svg'
+  import rollerIcon from '$lib/images/roller.svg'
+  import svelteIcon from '$lib/images/svelte.svg'
+
+  const downloads = [
+    {
+      name: 'Chrome',
+      url: 'https://chrome.google.com/webstore/detail/roller/kdcakfeidhfeilahlclgbnmpgebafjpm',
+      icon: chromeIcon,
+    },
+    {
+      name: 'Firefox',
+      url: 'https://addons.mozilla.org/af/firefox/addon/roller-scroll/',
+      icon: firefoxIcon,
+    },
+  ]
+</script>
+
+<svelte:head>
+  <title>Roller</title>
+  <meta
+    name="description"
+    content="Browser extension that allow auto scrolling of web pages, by pressing the mouse wheel button"
+  />
+</svelte:head>
+
+<main
+  class="flex min-h-screen w-full flex-col bg-gradient-to-br from-blue-200 to-sky-200"
+>
+  <div class="flex flex-grow flex-col px-4 py-8 text-center">
+    <div class="my-auto">
+      <img
+        src={rollerIcon}
+        class="mx-auto mb-6 max-w-[100px] drop-shadow sm:max-w-[120px]"
+        alt=""
+      />
+      <h1 class="mb-1 font-serif text-3xl text-sky-950">Roller</h1>
+      <p class="text-neutral-900 sm:text-lg">
+        Enable auto scrolling with the mouse wheel button on Linux
+      </p>
+
+      <div class="mt-10">
+        {#each downloads as download}
+          <a
+            href={download.url}
+            class="m-2 inline-flex items-center space-x-2 rounded bg-blue-500 p-4 text-white transition duration-300 hover:bg-blue-400 hover:shadow active:bg-blue-600 active:shadow-inner"
+          >
+            <img class="h-6 w-6 drop-shadow-sm" src={download.icon} alt="" />
+            <span>Download for {download.name}</span>
+          </a>
+        {/each}
+      </div>
+    </div>
+  </div>
+  <div class="p-4 text-center text-neutral-900">
+    Built with
+    <img class="inline h-4 w-4 align-baseline" src={svelteIcon} alt="Svelte" />
+    on
+    <a href="https://github.com/ubermanu/roller" class="hover:underline">
+      GitHub
+    </a>
+  </div>
+</main>
